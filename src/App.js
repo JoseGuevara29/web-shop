fe295ed11318003d012f0e9815d7d9e2a76c3049
joseguevara29 <guevarabus29@gmail.com>
2021-05-13 22:25:42 -0400

    setup the book shop from previous project

---
+++ src/App.js
@@ -0,0 +1,41 @@
+import React, { useState } from "react";
+import { Route } from "react-router-dom";
+import data from "./data";
+
+//context
+import { ProductContext } from "./context/ProductContext";
+import { CartContext } from "./context/CartContext";
+
+// Components
+import Navigation from "./components/Navigation";
+import Products from "./components/Products";
+import ShoppingCart from "./components/ShoppingCart";
+
+function App() {
+  const [products] = useState(data);
+  const [cart, setCart] = useState([]);
+
+  const addItem = (item) => {
+    // add the given item to the cart
+    setCart([...cart, item]);
+  };
+
+  return (
+    <div className="App">
+      <ProductContext.Provider value={{ products, addItem }}>
+        <CartContext.Provider value={{ cart }}>
+          <Navigation />
+          {/* Routes */}
+          <Route exact path="/">
+            <Products />
+          </Route>
+          <Route path="/cart">
+            <ShoppingCart />
+          </Route>
+        </CartContext.Provider>
+      </ProductContext.Provider>
+    </div>
+  );
+}
+
+export default App;
